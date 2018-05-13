7497466fcf8d9bc00903fb043130abd57289b31a
savyounts@gmail.com <savyounts@gmail.com>
2018-05-13 17:05:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray() {
-  var array = chocolateBars
   [element, ...array]
   return
 }
